56e1fa294a4aeac04c7268726c9ab9835478d6a0
Adit Malhotra <aditmalhotra8@gmail.com>
2023-09-29 01:33:30 +0530

    updated_main

--- tests/main.cpp
+++ tests/main.cpp
@@ -48,10 +48,12 @@ int main(int argc, char* argv[]) {
   if(code.size()==0) return 0;
 
   int var_count = 0;
+  int max_var = 0;
   for(const vector<string>& expr:code){
     if(expr[0]=="del") var_count --;
     else if(expr[0]=="ret") continue;
     else var_count++;
+    max_var = max(max_var,var_count);
   }
 
   if(code.back()[0] != "ret"){
@@ -60,7 +62,7 @@ int main(int argc, char* argv[]) {
   }
 
   try{
-    EPPCompiler eppc("targ.txt",var_count);
+    EPPCompiler eppc("targ.txt",max_var);
     eppc.compile(code);
   }
   catch(const exception& e){ 
